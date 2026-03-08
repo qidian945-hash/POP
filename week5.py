5b495de4b3325f18881e7adcc41225a80ace560f
qidian945-hash <qidian945@gmail.com>
2026-03-08 10:34:29 +0800

    Add files via upload

---
+++ week5.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+# 1. 显示包含名字的标题
+st.title("HELLO，I am [your name]")
+
+# 2. 显示子标题“关于我”
+# 使用 subheader 可以让页面结构更清晰
+st.header("about me")
+
+# 3. 使用 st.text() 显示关于你的 3 个事实
+st.text("1. i am lover Developer.")
+st.text("2. I really like writing automation tools with Python。")
+st.text("3. I am learning how to build interactive web applications through Streamlit。")
+
+# 4. 显示成功消息
+st.success("welcome to streamlit")
